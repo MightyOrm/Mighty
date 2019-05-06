9160a1880fb9ec65e46afc784000d826b408128b
MikeBeaton <mjsbeaton@gmail.com>
2019-05-06 09:52:02 +0100

    Added missing SP in SQL Server SP Tests defn's

--- tests/Sql/SqlServer/SQLServer_SPTests.sql
+++ tests/Sql/SqlServer/SQLServer_SPTests.sql
@@ -22,3 +22,19 @@ AS
     SELECT CurrencyCode, Name
     FROM Sales.Currency;
 GO
+
+IF OBJECT_ID ( 'dbo.TestVars', 'P' ) IS NOT NULL
+    DROP PROCEDURE dbo.TestVars;
+GO
+CREATE PROCEDURE dbo.TestVars
+	@v INT = NULL OUTPUT,
+	@w INT = NULL OUTPUT,
+	@x INT = NULL OUTPUT
+AS
+BEGIN
+	SET NOCOUNT ON;
+	SET @w = ISNULL(@w, 0) + 2
+	SET @v = @w - 1
+	SET @x = @w + 1
+END
+GO
