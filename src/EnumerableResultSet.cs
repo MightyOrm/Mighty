8445c90db3bc4780d7f0086ffe67b4d3ccdb5fc7
MikeBeaton <mjsbeaton@gmail.com>
2019-10-11 20:19:08 +0100

    Additional method to allow overriding mappings but not type in multiple result sets

--- src/EnumerableResultSet.cs
+++ src/EnumerableResultSet.cs
@@ -60,12 +60,53 @@ namespace Mighty
         }
 
         /// <summary>
-        /// Return the results in this result set as type <typeparamref name="T"/>.
+        /// Return the results in this result set as type <typeparamref name="T"/>, with current mappings etc.
         /// </summary>
         /// <returns></returns>
         public IEnumerable<T> Results()
         {
             return MightyInstance.QueryNWithParams<T>(null, (CommandBehavior)(-1), _connection, _outerReader);
+        }
+
+        /// <summary>
+        /// Return the results in this result set as type <typeparamref name="T"/>, with the potential to override mappings etc.
+        /// </summary>
+        /// <param name="tableName">Optional</param>
+        /// <param name="primaryKeys">Optional</param>
+        /// <param name="valueField">Optional</param>
+        /// <param name="sequence">Optional</param>
+        /// <param name="columns">Optional</param>
+        /// <param name="validator">Optional</param>
+        /// <param name="mapper">Optional - inherits the mapper from the parent instance of Mighty if null</param>
+        /// <param name="profiler">Optional - inherits the SQL profiler from the parent instance of Mighty if null</param>
+        /// <returns></returns>
+        public IEnumerable<T> Results(
+            string tableName = null,
+            string primaryKeys = null,
+            string valueField = null,
+            string sequence = null,
+            string columns = null,
+            Validator validator = null,
+            SqlNamingMapper mapper = null,
+            DataProfiler profiler = null)
+        {
+            var mightyT = new MightyOrm<T>(
+                        null,
+                        tableName,
+                        primaryKeys,
+#if KEY_VALUES
+                        valueField,
+#endif
+                        sequence,
+                        columns,
+                        validator,
+                        mapper ?? MightyInstance.SqlNamingMapper,
+                        profiler ?? MightyInstance.DataProfiler,
+                        new PresetsConnectionProvider(
+                            MightyInstance.ConnectionString,
+                            MightyInstance.Factory,
+                            MightyInstance.Plugin.GetType()));
+            return mightyT.QueryNWithParams<T>(null, (CommandBehavior)(-1), _connection, _outerReader);
         }
 
         /// <summary>
