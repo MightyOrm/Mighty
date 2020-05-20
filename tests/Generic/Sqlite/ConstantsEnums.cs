30316b51d5754c53e8a7d09db03bceb50aa287e2
MikeBeaton <mjsbeaton@gmail.com>
2020-05-20 15:32:05 +0100

    Update locations of db's

--- tests/Generic/Sqlite/ConstantsEnums.cs
+++ tests/Generic/Sqlite/ConstantsEnums.cs
@@ -8,7 +8,7 @@ namespace Mighty.Generic.Tests.Sqlite
     public static class TestConstants
     {
 #if (NETCOREAPP || NETSTANDARD)
-        public static readonly string ReadWriteTestConnection = @"Data Source=C:\Users\frans\Documents\ChinookDatabase1.4_Sqlite\Chinook_Sqlite_AutoIncrementPKs.sqlite;providerName=Microsoft.Data.Sqlite";
+        public static readonly string ReadWriteTestConnection = @"Data Source=C:\Users\mjsbeaton\Documents\ChinookDatabase1.4_Sqlite\Chinook_Sqlite_AutoIncrementPKs.sqlite;providerName=Microsoft.Data.Sqlite";
 #else
         public static readonly string ReadWriteTestConnection = "ReadWriteTests.ConnectionString.SQLite";
 #endif
