814b6ccbf09b96e18f0f0a82bc53ac84aa97f159
MikeBeaton <mjsbeaton@gmail.com>
2020-06-09 16:44:11 +0100

    Get the workable (i.e. not netcoreapp1.1) NUnitLite test runners working (Not sure how net40 tests are working in IDE without NET.Test.SDK or NUnit3TestAdapter?)

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,130 +1,23 @@
-﻿#if NET40 //|| NETCOREAPP
-using NUnit.Common;
-// NUnitLite (the console runner) isn't available for .NETCoreApp 1.1 (it should be for 1.0 and 2.0, via .NETStandard 1.3 and 1.6)
-using NUnitLite;
-using System;
-using System.Data.Common;
+﻿using System;
+
+#if !NETCOREAPP1_1
 using System.Reflection;
 
-using Mighty.Profiling;
-
-#if false //NET40
-// NB This CAN be loaded from NuGet pre-release package by name (currently SqlProfiler v0.0.1-alpha3),
-// or from co-loaded project.
-using SqlProfiler;
-using MightyTests.SOTests;
+// NUnitLite (the console runner) isn't available for .NETCoreApp 1.1 (it should be for 1.0 and 2.0, via .NETStandard 1.3 and 1.6)
+using NUnit.Common;
+using NUnitLite;
 #endif
 
 using MightyTests;
 
 namespace Mighty.Generic.Tests.NUnit.ConsoleRunner
 {
-#if false //NET40
-    /// <summary>
-    /// Make a simple link between <see cref="Mighty.Profiling.SqlProfiler"/> and <see cref="SqlProfiler.SimpleCommandWrapper"/>.
-    /// </summary>
-    /// <remarks>
-    /// Unless we want Mighty to depend on our specific profiling library, or the profiling library to depend on Mighty, then I
-    /// think we can't avoid something like this.
-    /// </remarks>
-    class MyProfiler : Mighty.Profiling.SqlProfiler
-    {
-        override public DbCommand Wrap(DbCommand command)
-        {
-            return new SqlProfiler.SimpleCommandWrapper(command);
-        }
-    }
-#endif
-
     class Program
     {
-#if false //NET40
-        static public void SOSubTest(dynamic da, dynamic db)
-        {
-            Console.WriteLine(da.Foo());
-            Console.WriteLine(da.Bar());
-            Console.WriteLine(db.Foo());
-            Console.WriteLine(db.Bar());
-        }
-        static public void SOTest()
-        {
-            dynamic d1 = new AStaticComponent();
-            dynamic d2 = new AStaticComponent();
-            dynamic d3 = new AStaticComponent();
-            dynamic d4 = new AStaticComponent();
-            Console.WriteLine(d1.Foo());
-            Console.WriteLine(d1.Bar());
-            Console.WriteLine(d2.Foo());
-            Console.WriteLine(d2.Bar());
-            Console.WriteLine(d3.Foo());
-            Console.WriteLine(d3.Bar());
-            Console.WriteLine(d4.Foo());
-            Console.WriteLine(d4.Bar());
-            SOSubTest(d1, d2);
-            SOSubTest(d3, d4);
-        }
-        static public void MyTest(object o)
-        {
-            var tc = new TestClass(o);
-            string r;
-
-            r = tc.A();
-            r = tc.C();
-            r = ((dynamic)tc).A();
-            r = ((dynamic)tc).B();
-            r = ((dynamic)tc).C();
-            r = ((dynamic)tc).D();
-
-            r = tc.W;
-            r = tc.Y;
-            r = ((dynamic)tc).W;
-            r = ((dynamic)tc).X;
-            r = ((dynamic)tc).Y;
-            r = ((dynamic)tc).Z;
-        }
-        static public void MyTest2(dynamic o)
-        {
-            Console.WriteLine(o.W);
-        }
-#endif
-
         static int Main(string[] args)
         {
-#if false //NET40
-            // Test the delegating meta-object
-            //SOTest();
-            //return 0;
-
-            //MyTest(new TestDynamic());
-            //MyTest(new TestDynamic());
-            //MyTest(new TestPOCO());
-            //return 0;
-
-            MyTest2(new TestDynamic());
-            MyTest2(new TestPOCO());
-            return 0;
-#endif
-
-#if false //NET40
-            // Test the profiler ... seems to be working now that we've fixed the DelegatingMetaObject (**NB** & TODO: Mighty itself is still using the broken one).
-            // Passing all tests on .NET 4.0, with or without the profiler, which is good.
-            MightyOrm.GlobalSqlProfiler = new MyProfiler();
-            //Mighty.Generic.Tests.Oracle.TableClasses.Departments.GlobalSqlProfiler = new MyProfiler();
-#endif
-
-#if false
-            // This is a test which was failing, just because it's an Oracle test which actually uses the dynamic properties thing.
-            // The fix was simple (needed to add BindingInstance.Public flag for a public Wrapped property).
-            new Mighty.Dynamic.Tests.Oracle.ReadWriteTests("Oracle.DataAccess.Client").Guid_Arg();
-
-            // Still some binding site confusion: okay, the binding I've written requires that once a SqlProfiler, always a SqlProfiler - but the GlobalSqlProfiler is only set for the dynamic version.
-            new Mighty.Generic.Tests.Oracle.ReadWriteTests("Oracle.DataAccess.Client").All_LimitSpecification();
-
-            return 0;
-#endif
-
             // Okay, this runs all the tests in the NUnit test runner.
-#if NET40 || NETCOREAPP
+#if !NETCOREAPP1_1
             return new AutoRun(
 #if NETFRAMEWORK
                 typeof(Program).Assembly
@@ -139,4 +32,3 @@ namespace Mighty.Generic.Tests.NUnit.ConsoleRunner
         }
     }
 }
-#endif
