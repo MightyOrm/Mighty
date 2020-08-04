7b8e01bd9d59da78e42155a7da513fed727bd0a7
MikeBeaton <mjsbeaton@gmail.com>
2020-08-04 16:31:28 +0100

    4.0.0-alpha-tests3 working (Debug is visual test runner, Release is NuGet tests package)

--- tests/Program.cs
+++ tests/Program.cs
@@ -1,35 +1,36 @@
-﻿using System;
-using System.Reflection;
+﻿#if RELEASE
+using System;
 
 #if !NETCOREAPP1_1
+using System.Reflection;
+
 // NUnitLite (the console runner) isn't available for .NETCoreApp 1.1 (it should be for 1.0 and 2.0, via .NETStandard 1.3 and 1.6)
 using NUnit.Common;
 using NUnitLite;
 #endif
 
-namespace Mighty
+using MightyTests;
+
+namespace Mighty.Generic.Tests.NUnit.ConsoleRunner
 {
     public class Program
     {
-        /// <summary>
-        /// This runs all the tests in the NUnit test runner.
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
         public static int Main(string[] args)
         {
-#if NETCOREAPP1_1
-            return 0;
-#else
+            // Okay, this runs all the tests in the NUnit test runner.
+#if !NETCOREAPP1_1
             return new AutoRun(
-                typeof(Mighty.MDebug)
-#if !NETFRAMEWORK
-                    .GetTypeInfo()
+#if NETFRAMEWORK
+                typeof(Program).Assembly
+#else
+                typeof(Program).GetTypeInfo().Assembly
 #endif
-                    .Assembly
                 )
                 .Execute(args, new ExtendedTextWrapper(Console.Out), Console.In);
+#else
+            return 0;
 #endif
         }
     }
 }
+#endif
